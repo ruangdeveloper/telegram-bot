56968f9381cc1be4bd6c989c18ccf002e0e6ac48
kykurniawan <contact.rizkykurniawan@gmail.com>
2022-03-28 19:51:33 +0800

    update

--- api/index.js
+++ api/index.js
@@ -12,10 +12,36 @@ const Bot = new Telegraf(BOT_TOKEN)
 const secretPath = `/api/webhook`
 
 Bot.start((ctx) => {
-    console.log(ctx)
-    ctx.reply("Halo Kak " + ctx.message.from.first_name, {
-        reply_to_message_id: ctx.message.message_id
-    })
+    try {
+        const text = `Halo kak ${ctx.message.from.first_name}. Aku adalah bot telegram yang bakal berikan informasi tentang Ruang Developer. Kalo kakak belum tau, Ruang Developer adalah sebuah situs blog yang berisi tentang informasi, tips & trik seputar dunia teknologi dan pengembangan aplikasi. Kakak bisa kunjungi di https://www.ruangdeveloper.com.`
+
+        ctx.reply(text, {
+            reply_to_message_id: ctx.message.message_id
+        })
+    } catch (error) {
+        ctx.reply("Maaf sepertinya sedang terjadi kesalahan", {
+            reply_to_message_id: ctx.message.message_id
+        })
+        console.log(error)
+    }
+})
+
+Bot.command("terbaru", async (ctx) => {
+    try {
+        let text = ""
+        const axiosResponse = await axios.get(WORDPRESS_URL + "/wp-json/wp/v2/posts?per_page=5")
+        axiosResponse.data.forEach((post) => {
+            text = text + post.title.rendered + "\n" + post.link + "\n\n"
+        })
+        ctx.reply(text, {
+            reply_to_message_id: ctx.message.message_id
+        })
+    } catch (error) {
+        ctx.reply("Maaf sepertinya sedang terjadi kesalahan", {
+            reply_to_message_id: ctx.message.message_id
+        })
+        console.log(error)
+    }
 })
 
 Bot.telegram.setWebhook(WEBHOOK_HOST + secretPath)
